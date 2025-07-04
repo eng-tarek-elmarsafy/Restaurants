5174310ff5d6f31fef67eaf7836a29b2049c90c2
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-07-04 20:49:37 +0300

    Refactor Supabase authentication: remove SupabaseAuthEmailServicesImpl, add SupabaseInitializing for initialization, and update project configuration with new Podfile.lock.

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:restaurants/constrains.dart';
 import 'package:restaurants/core/helper/on_generate_route_function.dart';
+import 'package:restaurants/core/services/supabase/supabase_initializing.dart';
 import 'package:restaurants/features/splash/presentation/views/splash_view.dart';
 import 'package:restaurants/generated/l10n.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Supabase.initialize(
-    url: 'https://ngzkhpjctpjtjmcfuiri.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nemtocGpjdHBqdGptY2Z1aXJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1MzY1ODQsImV4cCI6MjA2NzExMjU4NH0.aX_D9E2l-Z0tw9o6AUsSRa9yQVgLGg7OrsayIt_jTRM',
-  );
+  await SupabaseInitializing.initializing();
 
   runApp(const Restaurants());
 }
