9fd681d63ec72949cb20e0f28d9f72f001321eae
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-07-14 19:01:57 +0300

    refactor RetingModel

--- lib/features/restaurant_details/data/models/reting_model.dart
+++ lib/features/restaurant_details/data/models/reting_model.dart
@@ -1,10 +1,30 @@
 import 'package:restaurants/features/restaurant_details/domain/entites/reting_entity.dart';
 
 class RetingModel {
+  factory RetingModel.fromJson(Map<String, dynamic> data) {
+    return RetingModel(
+      id: data['id'],
+      createdIt: data['created_it'],
+      restaurantId: data['restaurant_id'],
+      userId: data['user_id'],
+      reting: data['reting'],
+      comment: data['comment'],
+    );
+  }
+  factory RetingModel.fromEntity(RetingEntity entity) {
+    return RetingModel(
+      id: entity.id,
+      createdIt: entity.createdIt,
+      restaurantId: entity.restaurantId,
+      userId: entity.userId,
+      reting: entity.reting,
+      comment: entity.comment,
+    );
+  }
   RetingModel({
     required this.id,
     required this.createdIt,
-    required this.restaurantAd,
+    required this.restaurantId,
     required this.userId,
     required this.reting,
     required this.comment,
@@ -12,39 +32,17 @@ class RetingModel {
 
   final String id;
   final String createdIt;
-  final String restaurantAd;
+  final String restaurantId;
   final String userId;
   final double reting;
   final String comment;
 
-  fromEntity(RetingEntity entity) {
-    return RetingModel(
-      id: entity.id,
-      createdIt: entity.createdIt,
-      restaurantAd: entity.restaurantId,
-      userId: entity.userId,
-      reting: entity.reting,
-      comment: entity.comment,
-    );
-  }
-
-  toJson() {
+  Map<String, dynamic> toJson() {
     return {
-      'restaurant_ad': restaurantAd,
+      'restaurant_id': restaurantId,
       'user_id': userId,
       'reting': reting,
       'comment': comment,
     };
-  }
-
-  fromJson(Map<String, dynamic> data) {
-    return RetingModel(
-      id: data['id'],
-      createdIt: data['created_it'],
-      restaurantAd: data['restaurant_id'],
-      userId: data['user_id'],
-      reting: data['reting'],
-      comment: data['comment'],
-    );
   }
 }
