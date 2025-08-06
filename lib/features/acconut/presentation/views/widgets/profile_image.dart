04656d2101ce9c1bd47aabeb28d21daad364bf0d
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-08-06 17:23:06 +0300

    Updates dependencies and fixes linter issues
    Updates `intl` and `skeletonizer` dependencies to the latest versions.
    
    This resolves some linter warnings by adjusting import paths to be relative.

--- lib/features/acconut/presentation/views/widgets/profile_image.dart
+++ lib/features/acconut/presentation/views/widgets/profile_image.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:restaurants/constrains.dart';
 import 'package:restaurants/core/services/shared_preferences.dart';
 import 'package:restaurants/core/style/app_style.dart';
