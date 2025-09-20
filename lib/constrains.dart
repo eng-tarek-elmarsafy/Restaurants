071ebd764793e3bbd6bce789b8acda833ee5b9b7
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-09-21 01:44:18 +0300

    ..

--- lib/constrains.dart
+++ lib/constrains.dart
@@ -7,7 +7,9 @@ const Color kSecondaryColor = Color(0xffdfad61);
 // const Color kNeutralColor = Color(0xffc8d6d9);
 const Color kNeutralColor = Color(0xffdecfb4);
 const double kBorderRadius = 16;
-
+const String kSupabaseUrl = 'https://ngzkhpjctpjtjmcfuiri.supabase.co';
+const String kSupabaseAnonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nemtocGpjdHBqdGptY2Z1aXJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1MzY1ODQsImV4cCI6MjA2NzExMjU4NH0.aX_D9E2l-Z0tw9o6AUsSRa9yQVgLGg7OrsayIt_jTRM';
 const String kUserName = 'userName';
 const String kEmail = 'email';
 const String kUserId = 'sub';
